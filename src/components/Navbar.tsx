14fcad52a4edae1279be6f612db92d8889d40100
reyxdz <reyfdenzo@outlook.com>
2026-03-27 09:33:48 +0800

    style(navbar): Capitalize Filipizen and align logo to the left

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,10 +5,10 @@ import './Navbar.css';
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="navbar__inner container">
+      <div className="navbar__inner">
         <Link to="/" className="navbar__brand">
           <img src={logo} alt="Filipizen Logo" className="navbar__logo" />
-          <span className="navbar__name">filipizen</span>
+          <span className="navbar__name">Filipizen</span>
         </Link>
       </div>
     </nav>
