211c818daf493bac14a83a8897f1c38b73d463e2
reyxdz <reyfdenzo@outlook.com>
2026-03-27 10:29:50 +0800

    Finished implementing Albay partner LGUs page

--- src/data/lgus.ts
+++ src/data/lgus.ts
@@ -1,3 +1,4 @@
+// AKLAN PROVINCE SEALS //
 import aklanProvince from '../assets/Images/provinces/aklan/aklanProvince.png';
 import altavas from '../assets/Images/provinces/aklan/altavas.png';
 import banga from '../assets/Images/provinces/aklan/banga.png';
@@ -9,6 +10,15 @@ import nabas from '../assets/Images/provinces/aklan/nabas.png';
 import newWashington from '../assets/Images/provinces/aklan/newWashington.png';
 import numancia from '../assets/Images/provinces/aklan/numancia.png';
 import tangalan from '../assets/Images/provinces/aklan/tangalan.png';
+
+// ALBAY PROVINCE SEALS //
+import albayProvince from '../assets/Images/provinces/albay/albayProvince.png';
+import bacacay from '../assets/Images/provinces/albay/bacacay.png';
+import legazpiCity from '../assets/Images/provinces/albay/legazpiCity.png';
+import ligaoCity from '../assets/Images/provinces/albay/ligaoCity.png';
+import malinao from '../assets/Images/provinces/albay/malinao.png';
+import tabacoCity from '../assets/Images/provinces/albay/tabacoCity.png';
+
 
 export interface LGU {
   name: string;
@@ -33,5 +43,13 @@ export const lgus: ProvinceLGUs = {
     { name: 'New Washington', seal: newWashington, slug: 'new-washington' },
     { name: 'Numancia', seal: numancia, slug: 'numancia' },
     { name: 'Tangalan', seal: tangalan, slug: 'tangalan' },
+  ],
+  albay: [
+    { name: 'Albay Province', seal: albayProvince, slug: 'albay-province' },
+    { name: 'Bacacay', seal: bacacay, slug: 'bacacay' },
+    { name: 'Legazpi City', seal: legazpiCity, slug: 'legazpi-city' },
+    { name: 'Ligao City', seal: ligaoCity, slug: 'ligao-city' },
+    { name: 'Malinao', seal: malinao, slug: 'malinao' },
+    { name: 'Tabaco City', seal: tabacoCity, slug: 'tabaco-city' },
   ],
 };
